baa43c444c05f7e60048425bf126dded1b2ffc1b
Patelhetu-177 <hetup9432@gmail.com>
2026-02-09 15:33:15 +0530

    resolved issue insidr api/inngest

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,19 @@
-import {serve} from "inngest/next";
-import {inngest} from "@/lib/inngest/client";
-import {sendSignUpEmail, sendDailyNewsSummary} from "@/lib/inngest/functions";
+'use server';
+
+import { serve } from 'inngest/next';
+import { inngest } from '@/lib/inngest/client';
+import { sendSignUpEmail, sendDailyNewsSummary } from '@/lib/inngest/functions';
 
 export const dynamic = 'force-dynamic';
 
 export const { GET, POST, PUT } = serve({
-    client: inngest,
-    functions: [sendSignUpEmail, sendDailyNewsSummary],
-})  
+  client: inngest,
+  functions: [sendSignUpEmail, sendDailyNewsSummary],
+  streaming: 'allow',
+  fetch: (url, options) => {
+    return fetch(url, {
+      ...options,
+      cache: 'no-store',
+    });
+  },
+});
