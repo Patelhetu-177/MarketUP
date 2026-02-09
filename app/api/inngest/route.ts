2ace21000ff16ad557f46307bf43c5c435148a46
Patelhetu-177 <hetup9432@gmail.com>
2026-02-09 15:51:35 +0530

    added force-dynamic for resolving server error

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,15 +1,10 @@
-import { serve } from 'inngest/next';
-import { inngest } from '@/lib/inngest/client';
-import { sendSignUpEmail, sendDailyNewsSummary } from '@/lib/inngest/functions';
+import {serve} from "inngest/next";
+import {inngest} from "@/lib/inngest/client";
+import {sendSignUpEmail, sendDailyNewsSummary} from "@/lib/inngest/functions";
 
-const handler = serve({
-  client: inngest,
-  functions: [sendSignUpEmail, sendDailyNewsSummary],
-  streaming: 'allow',
-});
+export const dynamic = "force-dynamic";
 
-export const GET = handler.GET;
-export const POST = handler.POST;
-export const PUT = handler.PUT;
-
-export const dynamic = 'force-dynamic';
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [sendSignUpEmail, sendDailyNewsSummary],
+})  
